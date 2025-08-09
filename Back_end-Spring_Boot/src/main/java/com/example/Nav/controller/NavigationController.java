eb36224629dd17539c37535cf4aa0cad9310e310
Divya Prakash <138959597+DivyaPrakash4@users.noreply.github.com>
2025-08-09 23:04:05 +0530

    Update NavigationController.java

--- Back_end-Spring_Boot/src/main/java/com/example/Nav/controller/NavigationController.java
+++ Back_end-Spring_Boot/src/main/java/com/example/Nav/controller/NavigationController.java
@@ -1,13 +1,10 @@
 package com.example.Nav.controller;
 
-
-
 import com.example.Nav.model.Graph;
 import com.example.Nav.model.Path;
 import com.example.Nav.service.DijkstraService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -22,13 +19,17 @@ public class NavigationController {
         this.dijkstraService = dijkstraService;
         this.graph = new Graph();
 
-        graph.addEdge("Lab", "Dean Off", 2);
+        // Define campus map edges
+        graph.addEdge("Lab", "Dean Off", 12);
         graph.addEdge("Lab", "Main Building", 4);
-        graph.addEdge("Dean Off", "Main Building", 1);
+        graph.addEdge("Dean Off", "Main Building", 8);
         graph.addEdge("Dean Off", "Ground", 7);
         graph.addEdge("Main Building", "Ground", 3);
-        graph.addEdge("Ground", "Library", 1);
-        graph.addEdge("Library", "Gate", 5);
+        graph.addEdge("Ground", "Library", 7);
+        graph.addEdge("Library", "Gate", 20);
+        graph.addEdge("Ground", "Lab", 5);
+        graph.addEdge("Main Building", "Gate", 9);
+
     }
 
     @GetMapping("/path")
